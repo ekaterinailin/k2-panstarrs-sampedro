400f2a62800e341bfb454d711b59ebe314c4d1fb
Ekaterina Ilin <ekaterina.ilin@posteo.de>
2017-12-11 13:40:50 +0100

    Working backup version for Code Review.

--- event_handling.py
+++ event_handling.py
@@ -1,47 +1,52 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#Kepler EPIC ID for the light curve I want to look at:
+
 objectid='205071984'
+
+#Read in data and convert to lists:
+
 time,flux_gap,error,flux_model=np.loadtxt('share/cross_match/union/M44_post_appa/'+objectid+'.txt',delimiter=',',unpack=True)
 istart, istop=np.loadtxt('share/cross_match/union/M44_post_appa/'+objectid+'_flares.txt',delimiter=',',unpack=True,dtype=np.dtype(np.int16))
 time,flux_gap,error,flux_model,istart, istop=list(time),list(flux_gap),list(error),list(flux_model),list(istart), list(istop)
 
+
+#Set up a plot with matplotlib:
+
 A = plt.figure(figsize=(5,1))
 ax = A.add_subplot(111)
-ax.plot(time, flux_gap,'o',alpha=0.8, lw=0.4, picker=5)#,ecolor='g')
-#, yerr=error
+ax.plot(time, flux_gap,'o',alpha=0.8, lw=0.4, picker=5) #observation
+plt.errorbar(time, flux_gap,yerr=error,alpha=0.8, lw=1) #statistical error
 for g,start in enumerate(istart):
-	plt.plot(time[istart[g]:istop[g]+1],flux_gap[istart[g]:istop[g]+1],color='red', lw=1)
+	plt.plot(time[istart[g]:istop[g]+1],flux_gap[istart[g]:istop[g]+1],color='red', lw=1) #detected flares in observations
 
-plt.plot(time, flux_model, 'blue', lw=0.5)
+plt.plot(time, flux_model, 'blue', lw=0.5) #model light curve from which the flare signatures deviate
 		
-plt.title('EPIC '+ objectid, fontsize=8)
-plt.xlabel('Time (BJD - 2454833 days)', fontsize=8)
-plt.ylabel(r'Flux ($e^-$ sec$^{-1}$)', fontsize=8)
+#Cosmetics
 
-plt.xticks(fontsize=8, rotation=0)
-plt.yticks(fontsize=8, rotation=0)
-xdur = max(time) - min(time)
-xdur0 = min(time)# + xdur/2.
-xdur1 = max(time)# + xdur/1.5
+plt.title('EPIC '+ objectid)
+plt.xlabel('Time (BJD - 2454833 days)')
+plt.ylabel(r'Flux ($e^-$ sec$^{-1}$)')
+
+#Plot range specification
+
+xdur0 = min(time)
+xdur1 = max(time)
 xdurok = np.where((time >= xdur0) & (time <= xdur1))
-xdurok=xdurok[0]
-
-plt.xlim(xdur0, xdur1) # only plot a chunk of the data
-print(xdurok)
+xdurok=xdurok[0] #xdurok is a tuple with a list: conversion needed
+plt.xlim(xdur0, xdur1) 
 plt.ylim(min([flux_gap[x] for x in xdurok]), max([flux_gap[x] for x in xdurok]))
-#plt.savefig('share/cross_match/union/M44_post_appa/'+objectid + '_lightcurve.png', dpi=300, bbox_inches='tight', pad_inches=0.02)
 
-def onpick(event):
-	print(type(event))
-	thisline = event.artist
-	print(type(thisline))
+def onpick(event):  #event => matplotlib.backend_bases.PickEvent
+	thisline = event.artist #matplotlib.lines.Line2D
 	xdata = thisline.get_xdata()
 	ydata = thisline.get_ydata()
 	ind = event.ind
 	points = tuple(zip(xdata[ind], ydata[ind]))
 	print('onpick points:', points)
+	return points
 
-A.canvas.mpl_connect('pick_event', onpick)
+f=A.canvas.mpl_connect('pick_event', onpick)
 plt.show()
 
