0279d42c41cde82d0a0b3633b7c788b4bda41a0c
Ekaterina Ilin <ekaterina.ilin@posteo.de>
2017-12-12 10:07:22 +0100

    Added functionality to the interactive plot. Pick flares from plot!

--- event_handling.py
+++ event_handling.py
@@ -2,27 +2,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #Kepler EPIC ID for the light curve I want to look at:
-
+cluster='M44'
 objectid='205071984'
 
 #Read in data and convert to lists:
 
-time,flux_gap,error,flux_model=np.loadtxt('share/cross_match/union/M44_post_appa/'+objectid+'.txt',delimiter=',',unpack=True)
-istart, istop=np.loadtxt('share/cross_match/union/M44_post_appa/'+objectid+'_flares.txt',delimiter=',',unpack=True,dtype=np.dtype(np.int16))
+time,flux_gap,error,flux_model=np.loadtxt('share/cross_match/union/'+cluster+'_post_appa/'+objectid+'.txt',delimiter=',',unpack=True)
+istart, istop=np.loadtxt('share/cross_match/union/'+cluster+'_post_appa/'+objectid+'_flares.txt',delimiter=',',unpack=True,dtype=np.dtype(np.int16))
 time,flux_gap,error,flux_model,istart, istop=list(time),list(flux_gap),list(error),list(flux_model),list(istart), list(istop)
 
 
 #Set up a plot with matplotlib:
 
-A = plt.figure(figsize=(5,1))
-ax = A.add_subplot(111)
-ax.plot(time, flux_gap,'o',alpha=0.8, lw=0.4, picker=5) #observation
+#A = plt.figure(figsize=(5,1))
+#ax = A.add_subplot(111)
+A, ax =plt.subplots()
+bx=ax.plot(time, flux_gap,'o',alpha=0.8, lw=0.4, picker=5) #observation
 plt.errorbar(time, flux_gap,yerr=error,alpha=0.8, lw=1) #statistical error
 for g,start in enumerate(istart):
 	plt.plot(time[istart[g]:istop[g]+1],flux_gap[istart[g]:istop[g]+1],color='red', lw=1) #detected flares in observations
 
 plt.plot(time, flux_model, 'blue', lw=0.5) #model light curve from which the flare signatures deviate
-		
+
 #Cosmetics
 
 plt.title('EPIC '+ objectid)
@@ -38,15 +39,70 @@ xdurok=xdurok[0] #xdurok is a tuple with a list: conversion needed
 plt.xlim(xdur0, xdur1) 
 plt.ylim(min([flux_gap[x] for x in xdurok]), max([flux_gap[x] for x in xdurok]))
 
-def onpick(event):  #event => matplotlib.backend_bases.PickEvent
+
+count=0
+myflare_start,myflare_stop,myflare_start_flux,myflare_stop_flux=[],[],[],[]
+
+
+def on_pick(event):  #event => matplotlib.backend_bases.PickEvent
+	global count, myflare_start, myflare_stop, myflare_start_flux, myflare_stop_flux
 	thisline = event.artist #matplotlib.lines.Line2D
 	xdata = thisline.get_xdata()
 	ydata = thisline.get_ydata()
 	ind = event.ind
-	points = tuple(zip(xdata[ind], ydata[ind]))
-	print('onpick points:', points)
-	return points
+	time = xdata[ind]
+	flux = ydata[ind]
+	print('onpick time:', time)
+	count+=1
 
-f=A.canvas.mpl_connect('pick_event', onpick)
+	if count%2==1:
+		myflare_start.append(time)
+		myflare_start_flux.append(flux)
+		plt.plot(myflare_start,myflare_start_flux,'o',alpha=0.8, lw=0.4,color='green')
+
+
+	elif count%2==0:
+		myflare_stop.append(time)
+		myflare_stop_flux.append(flux)
+		plt.plot(myflare_stop,myflare_stop_flux,'o',alpha=0.8, lw=0.4,color='red')
+		print('Press \"enter\" to confirm flare events. Press \"x\" to remove.')
+	
+	plt.draw()
+
+	return
+
+def on_key(event):
+	global count, myflare_start, myflare_stop, myflare_start_flux, myflare_stop_flux
+	myflares=open('share/cross_match/union/'+cluster+'_post_appa/'+objectid+'_my_flares.txt','a')
+	print('You pressed', event.key)
+	
+	if event.key=='enter':
+	
+		print('The following events are added to the list:\n')
+		for i in range(count//2):
+			line=str(myflare_start[i][0])+','+ str(myflare_stop[i][0])+'\n'
+			print(line)
+			myflares.write(line)
+	
+	elif event.key=='x':
+	
+		print('The following events are removed from the list:\n')
+		for i in range(count//2):
+			line=str(myflare_start[i][0])+','+ str(myflare_stop[i][0])+'\n'
+			print(line)
+	
+	count=0
+	myflare_start,myflare_stop,myflare_start_flux,myflare_stop_flux=[],[],[],[]
+
+
+			
+	myflares.close()
+	return
+
+
+#Connect event with string 'key_press_event' to function on_key:
+cid = A.canvas.mpl_connect('key_press_event', on_key) 
+#Connect event with string 'pick_event' to function on_pick:
+cid2=A.canvas.mpl_connect('pick_event', on_pick)#The FigureCanvas method mpl_connect() returns a connection id which is simply an integer.
 plt.show()
 
